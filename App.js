df928da042e57fcd607166bc9dcbbb9f6302dec3
Mariya Zaveri <mariyazaveri13@gmail.com>
2024-01-18 17:21:54 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
+
+  let x = 1;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Heyyy!</Text>
+      <Image source={{uri:"https://fastly.picsum.photos/id/339/200/300.jpg?hmac=rX8fDMMSNKZhzvcqJCduRuj33pXDgDlJiqOMqSuNwOo",
+        width:200,
+        height:300
+    }}></Image>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +19,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'center',
+    alignItems:'center'
   },
 });
